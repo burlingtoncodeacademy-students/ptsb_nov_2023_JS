a245a11d1c2afacdcdd14f53f2be53f76581e8f8
u4ik <insighteuphoric@gmail.com>
2023-11-30 20:18:03 -0500

    added dowhile loop

--- loops/while_loops.js
+++ loops/while_loops.js
@@ -15,12 +15,12 @@ let index = 0;
 let sum = 0;
 
 while (index < numArr.length) {
-  console.log(numArr[index]);
+  // console.log(numArr[index]);
   numArr[index] > 0 && (sum += numArr[index]);
   index++;
 }
 
-console.log(`The sum of all the positive integers is: ${sum}`);
+// console.log(`The sum of all the positive integers is: ${sum}`);
 
 let city = [
   "building",
@@ -37,10 +37,73 @@ let structure = city[0];
 let pos = 0;
 
 while (structure != "empty street") {
-  console.log(`Spiderman swings from the ${structure}`);
+  // console.log(`Spiderman swings from the ${structure}`);
   pos++;
 
   structure = city[pos];
 }
 
-console.log(`Spider man is landing in the ${structure}`);
+// console.log(`Spider man is landing in the ${structure}`);
+
+let spidyHP = 20;
+let badGuys = [
+  {
+    baddie: "Green Goblin",
+    hp: 15,
+  },
+  {
+    baddie: "Venom",
+    hp: 25,
+  },
+  {
+    baddie: "Stilt-man",
+    hp: 5,
+  },
+  {
+    baddie: "Thug",
+    hp: 0,
+  },
+];
+
+/*
+?   Do while
+
+    do {
+      ...code block
+    } while (expression)
+
+    - works just like a while loop with the exception that it will run our code block at least once.
+
+*/
+
+let random = Math.floor(Math.random() * badGuys.length);
+// console.log(random);
+let villain = badGuys[random];
+// console.log(villain);
+
+console.log(`${villain.baddie} has appeared.`);
+
+do {
+  if (villain.baddie === "Thug") {
+    console.log("Spider-Man webs the thug... EZPZ");
+    break;
+  }
+
+  console.log(`${villain.baddie} hits Spider-Man!`);
+  spidyHP--;
+  console.log("Spider-Man HP:", spidyHP);
+
+  let spidyHit = Math.floor(Math.random() * 3);
+  console.log(`Spider-Man hits ${villain.baddie} for ${spidyHit} damage`);
+
+  villain.hp -= spidyHit;
+  console.log("Villain HP", villain.hp);
+
+  if (spidyHP === 0) {
+    break;
+  }
+} while (villain.hp > 0);
+
+villain.hp <= 0
+  ? console.log(`Spider-Man captures ${villain.baddie}`)
+  : console.log(`${villain.baddie} has taken down Spider-Man`);
